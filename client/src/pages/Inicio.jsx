b850b44e9abb028366b1dc404817e8c1f22b7706
Martín Sebastian Bacalusso <mbacalusso@gmail.com>
2022-09-27 16:34:41 -0300

    Añadido boton de ordenamiento

--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,17 +1,21 @@
 /** @format */
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { prueba } from '../redux/actions';
+import { prueba } from "../redux/actions";
 
-import { Product } from '../components';
+import { Product } from "../components";
+import { Order } from "../components/Order/Order";
 
 export const Inicio = () => {
   const dispatch = useDispatch();
 
   const clothing = useSelector((state) => state.clothing);
+
+   
+
 
   useEffect(() => {
     console.log(clothing);
@@ -19,14 +23,17 @@ export const Inicio = () => {
 
   return (
     <>
-      <section className='section has-background-light is-clipped'>
-        <div className='container'>
-          <h2 className='title mb-16 mb-24-tablet'>Discover our products</h2>
-          <div className='mb-20 columns is-multiline'>
+      <section className="section has-background-light is-clipped">
+        <div className="container">
+          <div>
+            <h2 className="title mb-16 mb-24-tablet">Discover our products</h2>
+            <Order />
+          </div>
+          <div className="mb-20 columns is-multiline">
             <Product />
           </div>
-          <div className='has-text-centered'>
-            <a className='button is-primary' href='#'>
+          <div className="has-text-centered">
+            <a className="button is-primary" href="#">
               Show More
             </a>
           </div>
