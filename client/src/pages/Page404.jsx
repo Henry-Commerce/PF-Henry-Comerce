39c9893ecba42e4e861f8eecfd2e1eb54c1bc8cf
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-09-27 14:47:36 -0500

    Correcion Error className

--- client/src/pages/Page404.jsx
+++ client/src/pages/Page404.jsx
@@ -2,11 +2,13 @@
 
 export const Page404 = () => {
   return (
-    <section class='hero is-light is-fullheight'>
-      <div class='hero-body'>
-        <div class='container'>
-          <h1 class='title has-text-centered has-text-danger is-size-1'>404</h1>
-          <h2 class='subtitle has-text-centered '>
+    <section className='hero is-light is-fullheight'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title has-text-centered has-text-danger is-size-1'>
+            404
+          </h1>
+          <h2 className='subtitle has-text-centered '>
             OOPS, THE PAGE YOU ARE LOOKING FOR CAN'T BE FOUND!
           </h2>
           <div className='has-text-centered'>
