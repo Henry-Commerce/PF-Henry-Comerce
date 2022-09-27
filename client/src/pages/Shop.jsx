fc0df202e1ea2c60371f591f43a7aeed87de7cbc
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-09-26 23:17:43 -0500

    Configuracion Inicial

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -74,6 +74,7 @@ export const Shop = () => {
                 </div>
               </div>
             </div>
+
             <div className='column is-12 is-4-widescreen'>
               <div className='has-background-white p-6 p-16-desktop'>
                 <h3 className='title is-size-3 mb-6'>Cart totals</h3>
@@ -113,97 +114,3 @@ export const Shop = () => {
     </section>
   );
 };
-
-// <div className='columns is-vcentered is-multiline'>
-// <div className='column is-6-desktop is-7-tablet mb-6 mb-0-tablet'>
-//   <div className='columns is-vcentered is-multiline'>
-//     <div className='column is-4-tablet mb-3'>
-//       <div
-//         className='is-flex has-background-light is-justify-content-center is-align-items-center'
-//         style='width: 96px; height: 128px;'>
-//         <img
-//           className='image is-fullwidth'
-//           style='object-fit: contain;'
-//           src='https://moviesshopco.vtexassets.com/arquivos/ids/169080-800-800?v=637608365520970000&width=800&height=800&aspect=true'
-//           alt=''
-//         />
-//       </div>
-//     </div>
-//     <div className='column is-8'>
-//       <h3 className='subtitle mb-2 has-text-weight-bold'>
-//         Nike basketball ball
-//       </h3>
-//       <p className='mb-0 has-text-grey'>
-//         Maecenas 0.7 commodo sit
-//       </p>
-//     </div>
-//   </div>
-// </div>
-// <div className='column is-2 is-hidden-touch'>
-//   <p className='subtitle has-text-info mb-0 has-text-weight-bold'>
-//     $29.89
-//   </p>
-//   <span
-//     className='has-text-grey'
-//     style='text-decoration: line-through;'>
-//     $33.69
-//   </span>
-// </div>
-// <div className='column is-2-desktop is-3-tablet'>
-//   <div
-//     className='px-4 is-inline-flex is-align-items-center has-text-weight-bold'
-//     style='border: 1px solid #DBDDE1; border-radius: 8px;'>
-//     <button className='button is-ghost has-text-grey p-0'>
-//       <svg
-//         width='12'
-//         height='2'
-//         viewBox='0 0 12 2'
-//         fill='none'
-//         xmlns='http://www.w3.org/2000/svg'>
-//         <g opacity='0.35'>
-//           <rect
-//             x='12'
-//             width='2'
-//             height='12'
-//             transform='rotate(90 12 0)'
-//             fill='currentColor'></rect>
-//         </g>
-//       </svg>
-//     </button>
-//     <input
-//       className='input px-2 py-4 has-text-centered'
-//       style='width: 48px; border: none; box-shadow: none;'
-//       type='number'
-//       placeholder='1'
-//     />
-//     <button className='button is-ghost has-text-grey p-0'>
-//       <svg
-//         width='12'
-//         height='12'
-//         viewBox='0 0 12 12'
-//         fill='none'
-//         xmlns='http://www.w3.org/2000/svg'>
-//         <g opacity='0.35'>
-//           <rect
-//             x='5'
-//             width='2'
-//             height='12'
-//             fill='currentColor'></rect>
-//           <rect
-//             x='12'
-//             y='5'
-//             width='2'
-//             height='12'
-//             transform='rotate(90 12 5)'
-//             fill='currentColor'></rect>
-//         </g>
-//       </svg>
-//     </button>
-//   </div>
-// </div>
-// <div className='column is-2'>
-//   <p className='subtitle has-text-info has-text-weight-bold'>
-//     $29.89
-//   </p>
-// </div>
-// </div>
