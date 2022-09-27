ce6fab071b606c4c5d8f633c331e62451c26bd40
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-09-27 15:33:05 -0500

    Correcion ClassName

--- client/src/components/Order/Order.jsx
+++ client/src/components/Order/Order.jsx
@@ -1,16 +1,16 @@
 /** @format */
 
-import "./Order.scss";
+import './Order.scss';
 
 export const Order = () => {
   return (
-    <div class="order pb-4">
+    <div className='order pb-4'>
       <h3>Order by</h3>
-      <div class="select">
-        <select name="Price">
+      <div className='select'>
+        <select name='Price'>
           <option hidden>Price</option>
-          <option value="low">Lower price</option>
-          <option value="top">Higher price</option>
+          <option value='low'>Lower price</option>
+          <option value='top'>Higher price</option>
         </select>
       </div>
     </div>
