56740c6410d795e036a49de29822b7b510992fae
JuanMM94 <juanmartinmonasterio@gmail.com>
2022-09-27 19:07:20 -0300

    delete irrelevant comments

--- api/routes/clothing.js
+++ api/routes/clothing.js
@@ -1,14 +1,5 @@
 const { Router } = require('express');
-
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
 const router = Router();
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
-// ver index.js de api
 
 router.get('/', async (req, res) => {
   try {
