28a2ea180fc385edd43e5ba8e851cda316a724cc
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-09-27 14:38:48 -0500

    Page 404

--- client/src/pages/Page404.jsx
+++ client/src/pages/Page404.jsx
@@ -1,5 +1,21 @@
 /** @format */
 
 export const Page404 = () => {
-  return <div>Page404</div>;
+  return (
+    <section class='hero is-light is-fullheight'>
+      <div class='hero-body'>
+        <div class='container'>
+          <h1 class='title has-text-centered has-text-danger is-size-1'>404</h1>
+          <h2 class='subtitle has-text-centered '>
+            OOPS, THE PAGE YOU ARE LOOKING FOR CAN'T BE FOUND!
+          </h2>
+          <div className='has-text-centered'>
+            <a href='/' className='button is-primary '>
+              BACK TO HOMEPAGE
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
