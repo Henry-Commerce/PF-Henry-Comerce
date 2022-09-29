c28a5e280a0d6fa4b3142a28fa6473574d46fb07
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-09-29 12:27:09 -0500

    Add image

--- client/src/pages/admin/AddClothing.jsx
+++ client/src/pages/admin/AddClothing.jsx
@@ -1,128 +1,145 @@
 /** @format */
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Notify } from '../../components';
+import { toast } from 'react-toastify';
 
-import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import { uploadFile } from '../../firebase/config.js';
+import { FaDollarSign } from 'react-icons/fa';
+import { BiRename } from 'react-icons/bi';
 
 export const AddClothing = () => {
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await uploadFile(file);
+    console.log(result);
+  };
+
+  const notify = () =>
+    toast.success('🦄 Prenda añadida correctamente', {
+      position: 'bottom-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      progress: undefined,
+    });
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      category: '',
+      price: '',
+      stock: {
+        XS: 0,
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0,
+        XXL: 0,
+      },
+      image: '',
+      description: '',
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string('Name must be string').required(
+        'Name is a required field'
+      ),
+      category: Yup.boolean().oneOf([true], 'You must select any category'),
+      price: Yup.number().min(0).required('Price is required'),
+      image: Yup.string().required('You must upload an image'),
+      description: Yup.string().required('You must add a description'),
+    }),
+    onSubmit: (formData) => {
+      // console.log(formData);
+
+      handleReset();
+      notify();
+    },
+  });
+
   return (
-    <div
-      className='container box p-6
-                has-background-light'>
-      <h1
-        className='title has-text-centered
-                 has-text-success'>
-        GeeksforGeeks
-      </h1>
-      <h2 className='subtitle has-text-centered'>Sign Up Form</h2>
-      <form action=''>
+    <div className='container box p-6 has-background-light'>
+      <h1 className='title has-text-centered '>Add Product</h1>
+      <h2 className='subtitle has-text-centered'>
+        Fill in the details correctly
+      </h2>
+      <form action='' onSubmit={handleSubmit}>
         <div className='field'>
           <label className='label'>Name</label>
-          <div className='control'>
+          <div className='control has-icons-left has-icons-right'>
             <input
               className='input'
               type='text'
-              placeholder='Enter your name'
-            />
-          </div>
-        </div>
-
-        <div className='field'>
-          <label className='label'>Username</label>
-          <div
-            className='control has-icons-left
-                      has-icons-right'>
-            <input
-              className='input is-success'
-              type='text'
-              placeholder='Enter your username'
+              name='name'
+              placeholder='Enter product name'
             />
             <span className='icon is-small is-left'>
-              <i className='fas fa-user'></i>
+              <BiRename />
             </span>
           </div>
         </div>
 
         <div className='field'>
-          <label className='label'>Email</label>
-          <div
-            className='control has-icons-left
-                      has-icons-right'>
-            <input
-              className='input is-danger'
-              type='email'
-              placeholder='Enter your email'
-            />
-            <span className='icon is-small is-left'>
-              <i className='fas fa-envelope'></i>
-            </span>
-          </div>
-        </div>
-
-        <div className='field'>
-          <label className='label'>Password</label>
-          <div
-            className='control has-icons-left
-                      has-icons-right'>
-            <input
-              className='input is-danger'
-              type='password'
-              placeholder='Enter your password'
-            />
-            <span className='icon is-small is-left'>
-              <i className='fas fa-key'></i>
-            </span>
-          </div>
-        </div>
-
-        <div className='field'>
-          <label className='label'>Choose your Course</label>
+          <label className='label'>Choose category</label>
           <div className='control'>
             <div className='select'>
               <select>
-                <option>Java</option>
-                <option>C++</option>
-                <option>Python</option>
+                <option>T-shirt</option>
+                <option>Pants</option>
+                <option>Jacket</option>
+                <option>Caps</option>
               </select>
             </div>
           </div>
         </div>
 
         <div className='field'>
-          <label className='label'>Something Else</label>
+          <label className='label'>Price</label>
+          <div className='control has-icons-left has-icons-right'>
+            <input
+              className='input is-danger'
+              type='number'
+              placeholder='Enter the price of the garment'
+              name='price'
+            />
+            <span className='icon is-small is-left'>
+              <FaDollarSign />
+            </span>
+          </div>
+        </div>
+
+        <div className='field'>
+          <label className='label'>Image</label>
+          <input type='file' onChange={(e) => setFile(e.target.files[0])} />
+        </div>
+
+        <div className='field'>
+          <label className='label'>Product description</label>
           <div className='control'>
             <textarea
               className='textarea'
-              placeholder='Want to tell anything?'></textarea>
-          </div>
-        </div>
-
-        <div className='field'>
-          <div className='control'>
-            <label className='checkbox'>
-              <input type='checkbox' />I agree to the{' '}
-              <a href='#'>terms and conditions</a>
-            </label>
-          </div>
-        </div>
-
-        <div className='field'>
-          <div className='control'>
-            <label className='radio'>
-              <input type='radio' name='question' />
-              Yes
-            </label>
-            <label className='radio'>
-              <input type='radio' name='question' />
-              No
-            </label>
+              placeholder='Want to tell anything?'
+              name='description'
+            />
           </div>
         </div>
 
         <div className='field is-grouped'>
           <div className='control'>
-            <button className='button is-success'>Sign up</button>
+            <button className='button is-primary' type='submit'>
+              Add
+            </button>
           </div>
+
           <div className='control'>
-            <button className='button is-link is-light'>Cancel</button>
+            <button className='button is-link is-light'>Clear</button>
           </div>
         </div>
       </form>
