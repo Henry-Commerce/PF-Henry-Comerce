6147c5802a70ae9b2eb6bb9fd08a814bee5e430d
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-10-04 16:40:02 -0500

    Team

---
+++ client/src/pages/Team.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Team = () => {
+    return <div>Team</div>
+}
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,14 @@
 /** @format */
 
-export * from './Page404';
-export * from './auth/Login';
-export * from './auth/Register';
-export * from './Inicio';
-export * from './Products';
-export * from './News';
-export * from './Offers';
-export * from './Shop';
-export * from './admin/AddClothing';
-export * from './FAQ';
+export * from './Page404'
+export * from './auth/Login'
+export * from './auth/Register'
+export * from './Inicio'
+export * from './Products'
+export * from './News'
+export * from './Offers'
+export * from './Shop'
+export * from './admin/AddClothing'
+export * from './FAQ'
+export * from './Team'
+export * from './admin/AdminDashoard/AdminDashboard'
