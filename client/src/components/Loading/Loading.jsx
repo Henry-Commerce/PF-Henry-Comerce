60cb62a3eb4db6c51e60cd4266d8a5afc8cdc5e3
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-10-04 12:46:12 -0500

    Loading

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,5 +1,16 @@
 /** @format */
-
+import load from '../../assets/HenryLoading.gif'
 export const Loading = () => {
-  return <div>Loading</div>;
-};
+    return (
+        <>
+            <img
+                src={load}
+                alt='loading...'
+                style={{
+                    width: '100%',
+                    height: '100%',
+                }}
+            />
+        </>
+    )
+}
