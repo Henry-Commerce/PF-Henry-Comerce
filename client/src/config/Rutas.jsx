c4b05334253b8cbb1ec3fdf8a1d7637f67cb26d5
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-10-04 16:40:49 -0500

    team rute

--- client/src/config/Rutas.jsx
+++ client/src/config/Rutas.jsx
@@ -1,39 +1,43 @@
 /** @format */
 
-import { Route, Routes } from 'react-router-dom';
-import { ProductsDetails } from '../components';
+import { Route, Routes } from 'react-router-dom'
+import { ProductsDetails } from '../components'
 import {
-  Page404,
-  Login,
-  Register,
-  Inicio,
-  Products,
-  News,
-  Offers,
-  Shop,
-  AddClothing,
-  FAQ,
-} from '../pages';
+    Page404,
+    Login,
+    Register,
+    Inicio,
+    Products,
+    News,
+    Offers,
+    Shop,
+    AddClothing,
+    FAQ,
+    AdminDashboard,
+    Team,
+} from '../pages'
 
 export const Rutas = () => {
-  return (
-    <Routes>
-      <Route path='*' element={<Page404 />} />
+    return (
+        <Routes>
+            <Route path='*' element={<Page404 />} />
 
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/shop' element={<Shop />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/shop' element={<Shop />} />
 
-      <Route path='/' element={<Inicio />} />
-      <Route path='/news' element={<News />} />
-      <Route path='/offers' element={<Offers />} />
+            <Route path='/' element={<Inicio />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/offers' element={<Offers />} />
+            <Route path='/team' element={<Team />} />
 
-      <Route path='/products' element={<Products />} />
-      <Route path='/products/:id' element={<ProductsDetails />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/products/:id' element={<ProductsDetails />} />
 
-      <Route path='/faq' element={<FAQ />} />
+            <Route path='/faq' element={<FAQ />} />
 
-      <Route path='/admin/add' element={<AddClothing />} />
-    </Routes>
-  );
-};
+            <Route path='/admin' element={<AdminDashboard />} />
+            <Route path='/admin/add' element={<AddClothing />} />
+        </Routes>
+    )
+}
