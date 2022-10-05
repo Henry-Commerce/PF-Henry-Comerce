a4a9da025842e8b6ece8d6448a253fbbd3ad26c9
JuanMM94 <juanmartinmonasterio@gmail.com>
2022-10-05 15:36:56 -0300

    Tabs to Spaces

--- api/routes/checkout.js
+++ api/routes/checkout.js
@@ -3,38 +3,38 @@ const router = Router();
 
 router.post("/create_preference", (req, res) => {
 
-	let preference = {
-		items: [
-			{
-				title: req.body.description,
-				unit_price: Number(req.body.price),
-				quantity: Number(req.body.quantity),
-			}
-		],
-		back_urls: {
-			"success": "http://localhost:3001/api/feedback",
-			"failure": "http://localhost:3001/api/feedback",
-			"pending": "http://localhost:3001/api/feedback"
-		},
-		auto_return: "approved",
-	};
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      }
+    ],
+    back_urls: {
+      "success": "http://localhost:3001/api/feedback",
+      "failure": "http://localhost:3001/api/feedback",
+      "pending": "http://localhost:3001/api/feedback"
+    },
+    auto_return: "approved",
+  };
 
-	mercadopago.preferences.create(preference)
-		.then(function (response) {
-			res.json({
-				id: response.body.id
-			});
-		}).catch(function (error) {
-			console.log(error);
-		});
+  mercadopago.preferences.create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id
+      });
+    }).catch(function (error) {
+      console.log(error);
+    });
 });
 
 router.get('/feedback', function(req, res) {
-	res.json({
-		Payment: req.query.payment_id,
-		Status: req.query.status,
-		MerchantOrder: req.query.merchant_order_id
-	});
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id
+  });
 });
 
 module.exports = router;
