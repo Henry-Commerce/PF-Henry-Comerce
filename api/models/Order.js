05b8952e3f6806ce7570a10d400dce99dd019bb8
JuanMM94 <juanmartinmonasterio@gmail.com>
2022-10-10 16:40:42 -0300

    Order model & checkout

--- api/models/Order.js
+++ api/models/Order.js
@@ -2,7 +2,19 @@ const { Schema, model } = require("mongoose");
 
 const OrderSchema = new Schema(
   {
-    
+    id: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    payer: new Schema({
+      email: String,
+      name: String,
+      surname: String,
+    }),
   },
   {
     timestamps: true,
--- api/routes/checkout.js
+++ api/routes/checkout.js
@@ -1,47 +1,54 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const mercadopago = require('mercadopago');
-
-router.get('/create_preference', (req, res) => {
-  res.sendStatus(200);
-})
+const mercadopago = require("mercadopago");
+const OrderModel = require("../models/Order");
 
 router.post("/create_preference", (req, res) => {
-
-  let preference = {
-    items: req.body.map(element => {
-      return (
-        {
-          title: element.name,
-          description: element.size,
-          unit_price: Number(element.price),
-          quantity: Number(element.count)
-        }
-      )
+  const preference = {
+    items: req.body.map((element) => {
+      return {
+        title: element.name,
+        description: element.size,
+        unit_price: Number(element.price),
+        quantity: Number(element.count),
+      };
     }),
     back_urls: {
-      "success": "http://localhost:3001/api/checkout/feedback",
-      "failure": "http://localhost:3001/api/checkout/feedback",
-      "pending": "http://localhost:3001/api/checkout/feedback"
+      success: "http://localhost:5173/success",
+      failure: "http://localhost:5173/failure",
+      pending: "http://localhost:5173/pending",
     },
     auto_return: "approved",
   };
 
-  mercadopago.preferences.create(preference)
-    .then(function (response) {
-      res.json({
-        id: response.body.id
+  mercadopago.preferences
+    .create(preference)
+    .then((response) => {
+      const newOrder = new OrderModel({
+        id: response.body.id,
+        items: response.body.items,
+        payer: response.body.payer,
       });
-    }).catch(function (error) {
-      console.log(error);
+      newOrder
+        .save()
+        .then((data) => {
+          res.status(200).json(data);
+        })
+        .catch((error) => {
+          res.status(404).json(error);
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log("Mercadopago Preferences", error);
     });
 });
 
-router.get('/feedback', function(req, res) {
+router.get("/feedback", function (req, res) {
   res.json({
     Payment: req.query.payment_id,
     Status: req.query.status,
-    MerchantOrder: req.query.merchant_order_id
+    MerchantOrder: req.query.merchant_order_id,
   });
 });
 
