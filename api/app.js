7ec899f10a2c87ff7aaa5970aaaad7eb59a2304a
JuanMM94 <juanmartinmonasterio@gmail.com>
2022-10-11 18:00:14 -0300

    mercadopago .env

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ const app = express();
 const mercadopago = require('mercadopago');
 
 mercadopago.configure({
-	access_token: `TEST-7750625349320937-100410-ef40284f62a63e0c898e907b27eda501-128332427`
+	access_token: `${ACCESS_TOKEN}`
 });
 
 app.name = 'API';
