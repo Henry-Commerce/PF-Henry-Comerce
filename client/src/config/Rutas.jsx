3ac10e54cfc9c1927299fd694ae6bdb5cc422ae0
Martín Sebastian Bacalusso <mbacalusso@gmail.com>
2022-10-12 23:58:25 -0300

    Avances en el dashboard/usuario

--- client/src/config/Rutas.jsx
+++ client/src/config/Rutas.jsx
@@ -1,53 +1,56 @@
 /** @format */
 
-import { Route, Routes } from 'react-router-dom'
-import { ProductsDetails } from '../components'
+import { Route, Routes } from "react-router-dom";
+import { ProductsDetails } from "../components";
 import {
-    Page404,
-    Login,
-    Register,
-    Inicio,
-    Products,
-    News,
-    Offers,
-    Shop,
-    AddClothing,
-    FAQ,
-    AdminDashboard,
-    Team,
-    CheckOutSuccess,
-    CheckOutFailure,
-    CheckOutPending,
-    UserDashboard,
-} from '../pages'
+  Page404,
+  Login,
+  Register,
+  Inicio,
+  Products,
+  News,
+  Offers,
+  Shop,
+  AddClothing,
+  FAQ,
+  AdminDashboard,
+  Team,
+  CheckOutSuccess,
+  CheckOutFailure,
+  CheckOutPending,
+  UserDashboard,
+  UserConfig,
+} from "../pages";
 
 export const Rutas = () => {
-    return (
-        <Routes>
-            <Route path='*' element={<Page404 />} />
+  return (
+    <Routes>
+      <Route path="*" element={<Page404 />} />
 
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/shop' element={<Shop />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/shop" element={<Shop />} />
 
-            <Route path='/' element={<Inicio />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/offers' element={<Offers />} />
-            <Route path='/team' element={<Team />} />
+      <Route path="/" element={<Inicio />} />
+      <Route path="/news" element={<News />} />
+      <Route path="/offers" element={<Offers />} />
+      <Route path="/team" element={<Team />} />
 
-            <Route path='/products' element={<Products />} />
-            <Route path='/products/:id' element={<ProductsDetails />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:id" element={<ProductsDetails />} />
 
-            <Route path='/faq' element={<FAQ />} />
+      <Route path="/faq" element={<FAQ />} />
 
-            <Route path='/admin' element={<AdminDashboard />} />
-            <Route path='/admin/add' element={<AddClothing />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin/add" element={<AddClothing />} />
 
-            <Route path='/user' element={<UserDashboard />} />
+      <Route path="/user/:username" element={<UserDashboard />} />
 
-            <Route path='/success' element={<CheckOutSuccess />} />
-            <Route path='/failure' element={<CheckOutFailure />} />
-            <Route path='/pending' element={<CheckOutPending />} />
-        </Routes>
-    )
-}
+      <Route path="/user/config/:username" element={<UserConfig />} />
+
+      <Route path="/success" element={<CheckOutSuccess />} />
+      <Route path="/failure" element={<CheckOutFailure />} />
+      <Route path="/pending" element={<CheckOutPending />} />
+    </Routes>
+  );
+};
