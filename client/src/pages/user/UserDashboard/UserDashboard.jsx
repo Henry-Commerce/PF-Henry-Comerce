cc938023ef4e041f4b2831ab36d3afa0a44ab1e3
Martín Sebastian Bacalusso <mbacalusso@gmail.com>
2022-10-14 20:01:07 -0300

    UserDashboard muestra sus datos

--- client/src/pages/user/UserDashboard/UserDashboard.jsx
+++ client/src/pages/user/UserDashboard/UserDashboard.jsx
@@ -1,70 +1,102 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 import { HiIdentification, HiMail, HiHome, HiPhone } from "react-icons/hi";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
-import { getUser, checkAuth } from "../../../redux/actions/actions";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { checkAuth } from "../../../redux/actions/actions";
+import { useNavigate } from "react-router-dom";
 
 export const UserDashboard = () => {
   const [isActive, setisActive] = useState(false);
   const [isActive1, setisActive1] = useState(false);
 
-  const [numberUsers, setNumberUsers] = useState(false);
-  const [numberAdmins, setNumberAdmins] = useState(false);
-  const [numberAccounts, setNumberAccounts] = useState(false);
-  const [day, setDay] = useState(false);
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const { email } = useParams();
-
-  console.log(user);
+  let session = null;
+  const [data, setData] = useState("");
 
   useEffect(() => {
-    dispatch(getUser(email));
-  }, [dispatch, email]);
+    if (localStorage.getItem("authenticated")) {
+      const { authenticated, isAdmin } = JSON.parse(
+        localStorage.getItem("authenticated")
+      );
+      if (authenticated) {
+        if (isAdmin === false) {
+          navigate("/user");
+        }
+      } else {
+        navigate("/user");
+      }
+    } else {
+      navigate("/user");
+    }
+
+    if (localStorage.getItem("authenticated")) {
+      session = JSON.parse(localStorage.getItem("authenticated"));
+      dispatch(checkAuth(session));
+    }
+
+    const profile = async () => {
+      const { email, token } = session;
+      const user = await axios.get(
+        `http://localhost:3001/api/user/info/${email}`,
+        {
+          headers: { "x-access-token": `${token}` },
+        }
+      );
+
+      setData(user.data);
+      console.log("data", data);
+    };
+    profile();
+  }, []);
+
+  useEffect(() => {}, [data]);
 
   return (
-    <article className="panel">
-      <p className="panel-heading title is-3">Mi cuenta</p>
-      <p className="panel-tabs ">
-        <Link
-          to={`/user/${email}`}
-          onClick={() => {
-            setisActive(!isActive);
-            setisActive1(false);
-          }}
-          className={`${isActive ? "is-active" : ""}`}
-        >
-          MIS DATOS
-        </Link>
-        <Link
-          to={`/user/config/${email}`}
-          onClick={() => {
-            setisActive(false);
-            setisActive1(!isActive1);
-          }}
-          className={`${isActive1 ? "is-active" : ""}`}
-        >
-          EDITAR DATOS
-        </Link>
-      </p>
-      <span className="panel-block">
-        <HiIdentification className="title is-2 m-1" />
-        <p className="is-size-4">Nombre de usuario: {user.username}</p>
-      </span>
-      <span className="panel-block">
-        <HiHome className="title is-2 m-1" />
-        <p className="is-size-4">Pais: {user.country}</p>
-      </span>
-      <span className="panel-block">
-        <HiMail className="title is-2 m-1" />
-        <p className="is-size-4">Mail: {user.email}</p>
-      </span>
-      <span className="panel-block">
-        <HiPhone className="title is-2 m-1" />
-        <p className="is-size-4">Telefono: 12345678</p>
-      </span>
-    </article>
+    <>
+      <article className="panel">
+        <p className="panel-heading title is-3">Mi cuenta</p>
+        <p className="panel-tabs ">
+          <Link
+            to={`/user`}
+            onClick={() => {
+              setisActive(!isActive);
+              setisActive1(false);
+            }}
+            className={`${isActive ? "is-active" : ""}`}
+          >
+            MIS DATOS
+          </Link>
+          <Link
+            to={`/user/config`}
+            onClick={() => {
+              setisActive(false);
+              setisActive1(!isActive1);
+            }}
+            className={`${isActive1 ? "is-active" : ""}`}
+          >
+            EDITAR DATOS
+          </Link>
+        </p>
+        <span className="panel-block">
+          <HiIdentification className="title is-2 m-1" />
+          <p className="is-size-4">Nombre de usuario: {data.username}</p>
+        </span>
+        <span className="panel-block">
+          <HiHome className="title is-2 m-1" />
+          <p className="is-size-4">Pais: {data.country}</p>
+        </span>
+        <span className="panel-block">
+          <HiMail className="title is-2 m-1" />
+          <p className="is-size-4">Mail: {data.email}</p>
+        </span>
+        <span className="panel-block">
+          <HiPhone className="title is-2 m-1" />
+          <p className="is-size-4">Telefono: 12345678</p>
+        </span>
+      </article>
+    </>
   );
 };
