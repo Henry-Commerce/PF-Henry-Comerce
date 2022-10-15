a39782b7c25adabbc6c074a3a599976e79e5bcb9
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-10-15 17:36:27 -0500

    user

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,7 +7,7 @@ const ClothingModel = require('../models/Clothing');
 const jwt = require('jsonwebtoken');
 const { SECRET } = process.env;
 const { verifyToken, isAdmin } = require('../middlewares/utils');
-const mail= require("./add-ons/nodemailer") 
+const mail = require('./add-ons/nodemailer');
 
 /************************************************************************************************
  *                                                                                              *
@@ -262,23 +262,82 @@ router.put('/newadmin', [verifyToken, isAdmin], async (req, res) => {
   return res.json(newadmin);
 });
 
-
-router.post('/welcome', async (req, res)=> {     
-  const {name,email}=req.body
+router.post('/welcome', async (req, res) => {
+  const { name, email } = req.body;
   try {
-    const transporter=mail.transporter;
-    const mailwelcome=mail.mailWelcome(email,name)
+    const transporter = mail.transporter;
+    const mailwelcome = mail.mailWelcome(email, name);
     transporter.sendMail(mailwelcome, (error, info) => {
       if (error) {
-        console.log(error)
+        console.log(error);
       } else {
         console.log('Email enviado');
       }
     });
-    res.status(201).send("ok")
+    res.status(201).send('ok');
   } catch (error) {
-    console.log("error"+error)
+    console.log('error' + error);
   }
+});
+
+router.put('/edit/:email', verifyToken, async (req, res) => {
+  const { email } = req.params;
+  const { username, country } = req.body;
+  try {
+    if (email) {
+      const foundUser = await UserModel.findOne({ email: email });
+      if (!foundUser)
+        return res.status(404).json({ message: 'User not found' });
+      if (username && country) {
+        await UserModel.findOneAndUpdate(
+          { email: email },
+          { username, country }
+        );
+        return res.json({ message: 'Updated User' });
+      }
+      if (username && !country) {
+        await UserModel.findOneAndUpdate({ email: email }, { username });
+        return res.json({ message: 'Updated Username' });
+      }
+      if (!username && country) {
+        await UserModel.findOneAndUpdate({ email: email }, { country });
+        return res.json({ message: 'Updated Country' });
+      }
+      if (!username && !country)
+        return res.status(404).json({ mesagge: 'No data provided' });
+    } else {
+      return res.json({ message: "Email isn't provided" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ message: 'Internal error', error });
+  }
+});
+
+router.put('/edit/pass', verifyToken, async (req, res) => {
+  const { email, password } = req.body;
+  if (!email && !password)
+    return res.json({ message: "Expected info isn't provided" });
+  const foundUser = await UserModel.findOne({ email: email });
+  if (!foundUser) return res.json({ message: 'User not found' });
+  if (email === foundUser.email)
+    return res.json({ message: 'Email should be diferent from the last one' });
+  const matchPassword = await UserModel.comparePassword(
+    password,
+    foundUser.password
+  );
+  if (matchPassword)
+    return res.json({
+      message: 'Password should be difrent from the last one',
     });
+  // await UserModel.findOneAndUpdate(
+  //   { email: email },
+  //   {
+  //     email,
+  //     password: await UserModel.encyptPassword(password),
+  //   }
+  // );
+  res.json({ message: 'Updated password methods' });
+});
 
 module.exports = router;
