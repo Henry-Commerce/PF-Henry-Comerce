f12b47ddcba2beb62f7c28ac6e9f4809b17633b2
Martín Sebastian Bacalusso <mbacalusso@gmail.com>
2022-10-15 16:56:55 -0300

    Edit user

--- client/src/pages/user/UserConfig/UserConfig.jsx
+++ client/src/pages/user/UserConfig/UserConfig.jsx
@@ -1,15 +1,14 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { Notify } from "../../../components/Notify/Notify";
 import { toast } from "react-toastify";
 import "../UserDashboard/User.scss";
-import { useNavigate } from "react-router-dom";
-import { checkAuth } from "../../../redux/actions/actions";
+import { checkAuth, editUser } from "../../../redux/actions/actions";
+import { Loading } from "../../../components/Loading/Loading";
 
 export const UserConfig = () => {
   const [isActive, setisActive] = useState(false);
@@ -58,6 +57,8 @@ export const UserConfig = () => {
 
   useEffect(() => {}, [data]);
 
+  console.log(data);
+
   const success = () =>
     toast.success("Los cambios se han realizado con exito", {
       position: "top-center",
@@ -73,8 +74,6 @@ export const UserConfig = () => {
     initialValues: {
       username: "",
       country: "",
-      email: "",
-      phone: "",
       oldPassword: "",
       newPassword: "",
       confirm: false,
@@ -89,15 +88,17 @@ export const UserConfig = () => {
       newPassword: Yup.string()
         .min(5, "La contraseña debe tener al menos 5 caracteres")
         .max(15, "La contraseña debe tener al menos 15 caracteres"),
-      confirm: Yup.boolean().oneOf([true], "acepta gato"),
+      confirm: Yup.boolean().oneOf([true], "XD"),
     }),
     onSubmit: (formData) => {
       console.log("formdata", formData);
-      /* dispatch(editUser(formData)); */
+      dispatch(editUser(formData));
       handleReset();
       success();
     },
   });
+
+  console.log(formik.values);
 
   const {
     values,
@@ -110,169 +111,172 @@ export const UserConfig = () => {
   } = formik;
 
   return (
-    <article className="panel">
-      <p className="panel-heading title is-3">Mi cuenta</p>
-      <p className="panel-tabs">
-        <Link
-          to={`/user`}
-          onClick={() => {
-            setisActive(!isActive);
-            setisActive1(false);
-          }}
-          className={`${isActive ? "is-active" : ""}`}
-        >
-          MIS DATOS
-        </Link>
-        <Link
-          to={`/user/config`}
-          onClick={() => {
-            setisActive(false);
-            setisActive1(!isActive1);
-          }}
-          className={`${isActive1 ? "is-active" : ""}`}
-        >
-          EDITAR DATOS
-        </Link>
-      </p>
-      <span className="subtitle put is-3">
-        Los campos que no sean completados no sufriran cambios
-      </span>
-      <form action="" onSubmit={handleSubmit}>
-        <div className="field m-3">
-          <label className="label">Nombre de usuario</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nuevo nombre de usuario"
-              name="username"
-              autoComplete="off"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.username}
-            />
-            {errors.username && touched.username && (
-              <div className="has-text-danger pt-2">{errors.username}</div>
-            )}
+    <>
+      {!data.username && <Loading />}
+      {data.username && (
+        <div className="columns is-centered">
+          <div className="column is-7">
+            <article className="panel form-user">
+              <p className="panel-heading title is-3">Mi cuenta</p>
+              <p className="panel-tabs">
+                <Link
+                  to={`/user`}
+                  onClick={() => {
+                    setisActive(!isActive);
+                    setisActive1(false);
+                  }}
+                  className={`${isActive ? "is-active" : ""}`}
+                >
+                  MIS DATOS
+                </Link>
+                <Link
+                  to={`/user/config`}
+                  onClick={() => {
+                    setisActive(false);
+                    setisActive1(!isActive1);
+                  }}
+                  className={`${isActive1 ? "is-active" : ""}`}
+                >
+                  EDITAR DATOS
+                </Link>
+              </p>
+              <p className="notification is-info caution m-4 p-4">
+                Los campos que no sean completados no sufriran cambios
+              </p>
+              <form action="" onSubmit={handleSubmit}>
+                <div className="field m-3">
+                  <label className="label">Nombre de usuario</label>
+                  <div className="control inputs">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Nuevo nombre de usuario"
+                      name="username"
+                      autoComplete="off"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.username || data.username}
+                    />
+                    {errors.username && touched.username && (
+                      <div className="has-text-danger pt-2">
+                        {errors.username}
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="field m-3">
+                  <label className="label">Pais</label>
+                  <div className="select">
+                    <select
+                      name="country"
+                      value={values.country || data.country}
+                      onChange={handleChange}
+                    >
+                      <option hidden>Elegir un pais</option>
+                      <option>Argentina</option>
+                    </select>
+                  </div>
+                </div>
+
+                {/*  <div className="field m-3">
+              <label className="label">Telefono</label>
+              <div className="control inputs">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Nuevo telefono"
+                  name="phone"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.phone}
+                />
+              </div>
+            </div> */}
+
+                <div className="field m-3">
+                  <label className="label">Contraseña actual</label>
+                  <div className="control inputs">
+                    <input
+                      className="input"
+                      type="password"
+                      placeholder="Contraseña actual"
+                      name="oldPassword"
+                      autoComplete="off"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.oldPassword}
+                    />
+                    {errors.oldPassword && touched.oldPassword && (
+                      <div className="has-text-danger pt-2">
+                        {errors.oldPassword}
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="field m-3">
+                  <label className="label">Nueva contraseña</label>
+                  <div className="control inputs">
+                    <input
+                      className="input"
+                      type="password"
+                      placeholder="Nueva contraseña"
+                      name="newPassword"
+                      autoComplete="off"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.newPassword}
+                    />
+                    {errors.newPassword && touched.newPassword && (
+                      <div className="has-text-danger pt-2">
+                        {errors.newPassword}
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="field m-3">
+                  <div className="control">
+                    <label className="checkbox">
+                      <input
+                        className="m-1"
+                        type="checkbox"
+                        name="confirm"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      ¿Estas seguro que quieres realizar los cambios?
+                    </label>
+                    {errors.confirm && touched.confirm && (
+                      <div className="has-text-danger pt-2">
+                        {errors.confirm}
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="field is-grouped m-3">
+                  <div className="control">
+                    <button className="button is-primary m-3" type="submit">
+                      Aceptar
+                    </button>
+                  </div>
+
+                  <div className="control">
+                    <Link to={`/user`}>
+                      <button className="button is-link is-light m-3">
+                        Cancelar
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </form>
+            </article>
           </div>
         </div>
-
-        <div className="field m-3">
-          <label className="label">Pais</label>
-          <div className="select">
-            <select
-              name="country"
-              value={values.country}
-              onChange={handleChange}
-            >
-              <option hidden>Elegir un pais</option>
-              <option>Argentina</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="field m-3">
-          <label className="label">Mail</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nuevo mail"
-              name="email"
-              autoComplete="off"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-          </div>
-        </div>
-
-        <div className="field m-3">
-          <label className="label">Telefono</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nuevo telefono"
-              name="phone"
-              autoComplete="off"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.phone}
-            />
-          </div>
-        </div>
-
-        <div className="field m-3">
-          <label className="label">Contraseña actual</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Contraseña actual"
-              name="oldPassword"
-              autoComplete="off"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.oldPassword}
-            />
-            {errors.oldPassword && touched.oldPassword && (
-              <div className="has-text-danger pt-2">{errors.oldPassword}</div>
-            )}
-          </div>
-        </div>
-
-        <div className="field m-3">
-          <label className="label">Nueva contraseña</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nueva contraseña"
-              name="newPassword"
-              autoComplete="off"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.newPassword}
-            />
-            {errors.newPassword && touched.newPassword && (
-              <div className="has-text-danger pt-2">{errors.newPassword}</div>
-            )}
-          </div>
-        </div>
-
-        <div className="field m-3">
-          <div className="control">
-            <label className="checkbox">
-              <input
-                className="m-1"
-                type="checkbox"
-                name="confirm"
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              ¿Estas seguro que quieres realizar los cambios?
-            </label>
-            {errors.confirm && touched.confirm && (
-              <div className="has-text-danger pt-2">{errors.confirm}</div>
-            )}
-          </div>
-        </div>
-
-        <div className="field is-grouped m-3">
-          <div className="control">
-            <button className="button is-primary m-3" type="submit">
-              Aceptar
-            </button>
-          </div>
-
-          <div className="control">
-            <Link to={`/user`}>
-              <button className="button is-link is-light m-3">Cancelar</button>
-            </Link>
-          </div>
-        </div>
-      </form>
-    </article>
+      )}
+    </>
   );
 };
