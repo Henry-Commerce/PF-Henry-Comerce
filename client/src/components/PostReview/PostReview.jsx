cd1c336de8838db7c0ff699b35bac3672b5aa3de
JuanMM94 <juanmartinmonasterio@gmail.com>
2022-10-15 15:37:41 -0300

    PostReview prettier

--- client/src/components/PostReview/PostReview.jsx
+++ client/src/components/PostReview/PostReview.jsx
@@ -1,71 +1,66 @@
-import React, { useState } from "react";
 import Popup from "reactjs-popup";
-import {FaStar} from "react-icons/fa"
-import { postReview } from "../../redux/actions/actions";
-import { useParams } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { checkAuth } from "../../redux/actions/actions";
+import { useEffect, useState } from "react";
+import { checkAuth, postReview } from "../../redux/actions/actions";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, useParams } from "react-router-dom";
 
 function validateInput(form) {
-    let error =  {}
-    if(!form.title){
-     error.title = "Asignarle un titulo a su reseña"
-    }else if (form.title.length > 20){
-     error.title = "El titulo es demasiado largo, intentelo denuevo"
-    }
-    if(!form.rating){
-      error.rating = "Tiene que seleccionar una dificultad"
-   }
-   if(!form.description){
-     error.description = "Se debe asignar una descripcion de prenda"
-   }else if(form.description.length > 400){
-     error.description = "Ha soprepasado el limite de caracteres permitidos"
-   }else if(form.description.length < 10){
-     error.description = "La descripcion es muy corta"
-   }
-   return error
-   }
+  let error = {};
+  if (!form.title) {
+    error.title = "Asignarle un titulo a su reseña";
+  } else if (form.title.length > 20) {
+    error.title = "El titulo es demasiado largo, intentelo denuevo";
+  }
+  if (!form.rating) {
+    error.rating = "Tiene que seleccionar una dificultad";
+  }
+  if (!form.description) {
+    error.description = "Se debe asignar una descripcion de prenda";
+  } else if (form.description.length > 400) {
+    error.description = "Ha soprepasado el limite de caracteres permitidos";
+  } else if (form.description.length < 10) {
+    error.description = "La descripcion es muy corta";
+  }
+  return error;
+}
 
 export const PostReview = () => {
-const dispatch = useDispatch()
-const {id} = useParams()
-const colors ={
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const colors = {
     orange: "#FFBA5A",
-    grey:"#a9a9a9"
-}
-const [star, setStar] = useState(null)
-const stars = Array(5).fill(0)
-const [currentValue, setCurrentValue] = useState(0)
-const [hoverValue,setHoverValue] = useState(undefined)
+    grey: "#a9a9a9",
+  };
 
-const authentication = localStorage.getItem("authenticated")
+  const [star, setStar] = useState(null);
+  const stars = Array(5).fill(0);
+  const [currentValue, setCurrentValue] = useState(0);
 
-/* MARTIN */
-const navigate = useNavigate();
-  let session = null;
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const authentication = localStorage.getItem("authenticated");
+  const detail = useSelector((state) => state.detail);
+
+  const navigate = useNavigate();
+  let session;
   const [data, setData] = useState("");
+
+  const [form, setForm] = useState({
+    product: "",
+    isEditing: false,
+    user: "",
+    title: "",
+    description: "",
+    rating: 0,
+  });
 
   useEffect(() => {
     if (localStorage.getItem("authenticated")) {
       const { authenticated, isAdmin } = JSON.parse(
         localStorage.getItem("authenticated")
       );
-      if (authenticated) {
-        if (isAdmin === false) {
-          navigate("/user");
-        }
-      } else {
-        navigate("/user");
-      }
-    } else {
-      navigate("/user");
-    }
-
-    if (localStorage.getItem("authenticated")) {
       session = JSON.parse(localStorage.getItem("authenticated"));
       dispatch(checkAuth(session));
     }
@@ -85,122 +80,131 @@ const navigate = useNavigate();
     profile();
   }, []);
 
-  useEffect(() => {}, [data]);
+  useEffect(() => {
+    setForm({
+      ...form,
+      user: data.username,
+      product: detail.name,
+    });
+  }, [data, detail]);
 
-/*  */
+  const userPerson = useSelector((e) => e.user);
 
+  const [error, setError] = useState({});
 
-const userPerson = useSelector((e) => e.user);
-
-console.log(data);
-
-const [error, setError] = useState({});
-
-
-const [form,setForm] = useState({
-    isEditing: false,
-    user: data.username,
-    title:"",
-    description:"",
-    rating: 0
-})
-
-
-function handleChange(e) {
+  function handleChange(e) {
     e.preventDefault();
     setForm({
       ...form,
       [e.target.name]: e.target.value,
-    }); 
-}
+    });
+  }
 
-const handleSubmit =(e) => {
-    if(form.title === "" ||
-    error.title ||
-    form.description === "" ||
-    error.description ||
-    form.rating === 0 ||
-    error.rating
-    ){
-      setError(validateInput({...form,[e.target.name]: e.target.value}))
-         e.preventDefault();
+  const handleSubmit = (e) => {
+    if (
+      form.title === "" ||
+      error.title ||
+      form.description === "" ||
+      error.description ||
+      form.rating === 0 ||
+      error.rating
+    ) {
+      setError(validateInput({ ...form, [e.target.name]: e.target.value }));
+      e.preventDefault();
     } else {
-    dispatch(postReview(id, form));
-    e.preventDefault();
+      dispatch(postReview(id, form));
+      e.preventDefault();
+      alert("Juanma gato");
+      navigate(-1);
+      data.reviews.push(form);
     }
-}
+  };
 
-
-
-    return (
-        <Popup
+  return (
+    <Popup
       className=""
       trigger={
         <section>
-          <button
-            className="button is-warning"
-            
-          >
-            Dar reseña
-          </button>
+          <button className="button is-warning">Dar reseña</button>
         </section>
       }
       modal
       nested
     >
-       {(close) => (
+      {(close) => (
         <div className="box widtht is-align-content-stretch">
-            <form onSubmit={(e) => handleSubmit(e)} autoComplete="off">
+          <form onSubmit={(e) => handleSubmit(e)} autoComplete="off">
             <div className="columns is-centered">
-                <div className="column has-text-centered is-4 pt-5">
+              <div className="column has-text-centered is-4 pt-5">
                 <h1>Titulo de la reseña</h1>
-                <input name="title" value={form.title}  onChange={(e) => handleChange(e)}className="input" type="text" placeholder="Text input"/>
+                <input
+                  name="title"
+                  value={form.title}
+                  onChange={(e) => handleChange(e)}
+                  className="input"
+                  type="text"
+                  placeholder="Text input"
+                />
                 {error.title && <p className="red">{error.title}</p>}
-                </div>
+              </div>
             </div>
             <div className="columns is-centered">
-                <div className="column has-text-centered is-12 pt-5">
-                    <h1>Rating</h1>
-                    {stars.map((_, index) => {
-                        const ratingValue = index + 1
-                        const handleClick = value => {
-                            setCurrentValue(value)
-                            setStar(ratingValue)
-                            form.rating = star
-                        }
-                        return (
-                            <FaStar
-                            key={index}
-                            value={form.rating}
-                            size={24}
-                            style={{
-                                marginRight: 10, 
-                                cursor: "pointer"
-                            }}
-                            color={(hoverValue || currentValue) > index ? colors.orange : colors.grey} 
-                            onClick={() => handleClick(index + 1)}
-                               
-                            />
-                        )
-                    })}
-                    {error.rating && <p className="red">{error.rating}</p>}
-                </div>
+              <div className="column has-text-centered is-12 pt-5">
+                <h1>Rating</h1>
+                {stars.map((_, index) => {
+                  const ratingValue = index + 1;
+                  const handleClick = (value) => {
+                    setCurrentValue(value);
+                    setStar(ratingValue);
+                    form.rating = star;
+                  };
+                  return (
+                    <FaStar
+                      key={index}
+                      value={form.rating}
+                      size={24}
+                      style={{
+                        marginRight: 10,
+                        cursor: "pointer",
+                      }}
+                      color={
+                        (hoverValue || currentValue) > index
+                          ? colors.orange
+                          : colors.grey
+                      }
+                      onClick={() => handleClick(index + 1)}
+                    />
+                  );
+                })}
+                {error.rating && <p className="red">{error.rating}</p>}
+              </div>
             </div>
             <div className="columns is-centered">
-                <div className="column has-text-centered is-9 pt-5">
-                    <h1>Descripcion sobre el producto</h1>
-                <textarea value={form.description || ""} name="description" onChange={(e) => handleChange(e)} className="textarea" placeholder="Describe tu experiencia de nuestro producto"></textarea>
-                {error.description && <p className="red">{error.description}</p>}
-                </div>
+              <div className="column has-text-centered is-9 pt-5">
+                <h1>Descripcion sobre el producto</h1>
+                <textarea
+                  value={form.description || ""}
+                  name="description"
+                  onChange={(e) => handleChange(e)}
+                  className="textarea"
+                  placeholder="Describe tu experiencia de nuestro producto"
+                ></textarea>
+                {error.description && (
+                  <p className="red">{error.description}</p>
+                )}
+              </div>
             </div>
-            
+
             <div className="columns is-centered">
-            <div className="column has-text-centered is-12 pt-5">
-                <button type="submit" className="button is-warning">Añadir reseña</button>
-                </div>
+              <div className="column has-text-centered is-12 pt-5">
+                <button type="submit" className="button is-warning">
+                  Añadir reseña
+                </button>
+              </div>
             </div>
-            </form>
+          </form>
         </div>
-       )}
-    </Popup>
       )}
+    </Popup>
+  );
+};
